3709025a7e64aeee9aaa9af387943bba403825e3
Adithya <86290692+adithyahere01@users.noreply.github.com>
2021-07-28 14:19:33 +0530

    Add files via upload

---
+++ FAQ Page/app.js
@@ -0,0 +1,26 @@
+//const btns = document.querySelectorAll('.question-btn');
+
+//travesing the dom
+//btns.forEach(function(btn){
+  // btn.addEventListener('click', function(e){
+  //   const styles = e.currentTarget.parentElement.parentElement;
+//})
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){
+
+  const btn = question.querySelector('.question-btn');
+
+  btn.addEventListener('click', function(){
+     questions.forEach(function(item){
+       if(item!==question){
+           item.classList.remove('show-text')
+       }
+     });
+
+
+
+    question.classList.toggle('show-text')
+  });
+});
